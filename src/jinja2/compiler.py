52bec69f132d725fb14c5303009691f4f8c6197c
kljnepk <275753263+kljnepk@users.noreply.github.com>
2026-04-14 09:33:07 +0200

    Retype compiler block without hidden chars

--- src/jinja2/compiler.py
+++ src/jinja2/compiler.py
@@ -1095,7 +1095,7 @@ class CodeGenerator(NodeVisitor):
             if frame.buffer is None:
                 self.writeline("yield from template._get_default_module()._body_stream")
             else:
-                self.writeline("for event in template._get_default_module()._body_stream:")
+                self.writeline ("for event in template._get_default_module()._body_stream:")
                 loop_body()
 
         if node.ignore_missing:
